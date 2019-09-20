290d42ed8509b024af34f421cc0fd69b03764b53
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-20 19:49:36 +0200

    Added new method to components.

--- src/App.js
+++ src/App.js
@@ -14,28 +14,52 @@ const app = props => {
         otherState: 'some other value'
     });
 
-    const switchNameHandler = () => {
+    const switchNameHandler = (newName) => {
         setPersonsState({
             persons: [
-                {name: "Robert", age: 27},
+                {name: newName, age: 27},
                 {name: "Jack", age: 27},
                 {name: "Kate", age: 26}
             ]
         });
     };
+
+    const nameChangedHandler = (event) => {
+        setPersonsState({
+            persons: [
+                {name: "Sawyer", age: 27},
+                {name: event.target.value, age: 27},
+                // event.target.value pobiera wartość z inputa z Person i wstawia zawartość w miejsce name w tym componencie
+                {name: "Kate", age: 26}
+            ]
+        });
+    };
+    const styles = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    };
         return (
             <div className="App">
                 <h1>Hi, This is a Robert Sawyer's React Application</h1>
                 <p>Hello, Mr. Clean!</p>
-                <button onClick={switchNameHandler}>Switch name</button>
+                <button
+                    style={styles}
+                    onClick={switchNameHandler.bind(this, 'Bob')}>Switch name</button>
+                <button onClick={() => switchNameHandler('Sawyer!')}>Switch name alternative way</button>
+                {/*poprzez bind możemy wykorzystać argument dostarczany do switchNameHandler*/}
+
                 <Person
                     name={personsState.persons[0].name}
                     age={personsState.persons[0].age}
                 />
                 <Person
                     name={personsState.persons[1].name}
-                    age={personsState.persons[1].age}>
-                    I am a Doctor
+                    age={personsState.persons[1].age}
+                    click={switchNameHandler.bind(this, 'Bobby')}
+                    changed={nameChangedHandler}>I am a Doctor
                 </Person>
                 <Person
                     name={personsState.persons[2].name}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I am {props.name} and I'm {props.age} years old</p>
+        <div className="Person">
+            <p onClick={props.click}>I am {props.name} and I'm {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
     // <p>Hey, this is my favourite leaf! I am {Math.floor(Math.random() * 30)} years old</p>
@@ -13,6 +15,9 @@ const person = (props) => {
     //Gdy chcemy dodać jakiś element zdefiniowany gdzies indziej (np w App.js) używamy do tego nawiasów klamrowych i metody children
     //Przekazany w tn sposób element powinien znajdować się między tagami Person w App co czyni go jego dzieckiem. Cały return zamykamy
     // w nawiasach zwykłych.
+
+    //Wyjaśnienie: onClick - dodaliśmy click jako properties w App, więc odnosimy się do niego tutaj za pomocą props, tak samo jak w przypadku
+    //name, age i children.
 }
 
 export default person;
