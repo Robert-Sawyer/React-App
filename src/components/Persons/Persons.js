fbd9914185b27e3764e1acba9eda02ca72aab04f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-15 23:10:19 +0200

    Modified components Person and Persons into class-based components.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person/Person';
 import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering....');
-    return props.persons.map((person, index) => {
-        return (
-            <ErrorBoundary key={person.id}>
-                {/*key jest potrzebne dla reacta i musi być unikalne*/}
-                <Person
-                    click={() => props.clicked(index)}
-                    name={person.name}
-                    age={person.age}
-                    changed={(event) => props.changed(event, person.id)}
-                />
-            </ErrorBoundary>
-        );
-    });
+class Persons extends Component {
+    render() {
+        console.log('[Persons.js] rendering....');
+        return this.props.persons.map((person, index) => {
+            return (
+                <ErrorBoundary key={person.id}>
+                    {/*key jest potrzebne dla reacta i musi być unikalne*/}
+                    <Person
+                        click={() => this.props.clicked(index)}
+                        name={person.name}
+                        age={person.age}
+                        changed={(event) => this.props.changed(event, person.id)}
+                    />
+                </ErrorBoundary>
+            );
+        });
+    }
+
+
 };
 
-export default persons;
+export default Persons;
