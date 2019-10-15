4835246c09890f6ed8fa89cfbacf5dc79e2b7b25
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-15 23:28:15 +0200

    Added Update Lifecycle methods to Persons and removed one method from App.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,6 +4,33 @@ import Person from './Person/Person';
 import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
 
 class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+
+    componentWillReceiveProps(props) {
+        console.log('[Persons.js] componentWillReceiveProps', props);
+    }
+
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     console.log('[Persons.js] shouldComponentUpdate');
+    //     return true;
+    // }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return {message: 'Snapshot'};
+    }
+
+    // componentWillUpdate() {
+    // }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
     render() {
         console.log('[Persons.js] rendering....');
         return this.props.persons.map((person, index) => {
